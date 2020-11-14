31bf4a5815c02f505f8a6442345b120ec7a0de86
rugwiro <rugwirobaker@gmail.com>
2020-11-14 18:44:47 +0200

    Fix:add forgetten code

--- payment/token.go
+++ payment/token.go
@@ -13,6 +13,15 @@ type Token struct {
 	Expires time.Time
 }
 
+// TokenKey is the key to use with the context.WithValue
+// function to associate an Token value with a context.
+type TokenKey struct{}
+
+// TokenSource returns a token.
+type TokenSource interface {
+	Token(context.Context) (*Token, error)
+}
+
 // AuthService handles authentication to the underlying API
 type AuthService interface {
 	// Login with id and secret to the underlying API and get an JWT token
@@ -20,4 +29,15 @@ type AuthService interface {
 
 	// Refresh the oauth2 token
 	Refresh(ctx context.Context, token *Token, force bool) (*Token, *Response, error)
+}
+
+// WithContext returns a copy of parent in which the token value is set
+func WithContext(parent context.Context, token *Token) context.Context {
+	return context.WithValue(parent, TokenKey{}, token)
+}
+
+// TokenFrom returns the login token rom the context.
+func TokenFrom(ctx context.Context) *Token {
+	token, _ := ctx.Value(TokenKey{}).(*Token)
+	return token
 }
