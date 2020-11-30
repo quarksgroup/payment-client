9adae956d11536c9762d8b75201e60d2a7d174c3
rugwiro <rugwirobaker@gmail.com>
2020-11-30 11:09:21 +0200

    Fix: fdi payment request invalid field input
    * fix `Channel` field input.
    * add `payment.Provider` field.

--- payment/payment.go
+++ payment/payment.go
@@ -2,11 +2,15 @@ package payment
 
 import "context"
 
+// Provider is the supported payment channel
+type Provider string
+
 // Payment represents as single transaction
 type Payment struct {
-	ID     string
-	Amount float64
-	Wallet string
+	ID       string
+	Amount   float64
+	Wallet   string
+	Provider Provider
 }
 
 // Status reports the status of a requested transaction
