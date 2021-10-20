47af256333aeb5b8121d529907c230e9eb32842f
iradukunda1 <iradujdd4@gmail.com>
2021-10-20 18:34:58 +0200

    create check current balance package-service

--- payment/balance.go
+++ payment/balance.go
@@ -1,9 +1,30 @@
 package payment
 
+import "context"
+
 // TODO: impelement balance service
 
+// Account  ...
+type Account struct {
+	Currency         string `json:"currency"`
+	BalanceAvailable uint64 `json:"balanceAvailable"`
+	BalanceActual    uint64 `json:"balanceActual"`
+}
+
+// Data  ...
+type Data struct {
+	Date     string    `json:"date"`
+	Accounts []Account `json:"accounts"`
+}
+
 // Balance ...
-type Balance struct{}
+type Balances struct {
+	Status string `json:"status"`
+	Data   Data   `json:"data"`
+}
 
 // BalanceService ...
-type BalanceService interface{}
+type BalanceService interface {
+	// Balance returns balanceInfo about it.
+	Balance(context.Context) (*Balances, *Response, error)
+}
