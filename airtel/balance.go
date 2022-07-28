845d303cc811f281756c64c106c48717ec737e40
iradukunda1 <iradujdd4@gmail.com>
2022-07-28 17:39:55 +0200

    add required headers in account balance check

--- airtel/balance.go
+++ airtel/balance.go
@@ -2,6 +2,7 @@ package airtel
 
 import (
 	"context"
+	"net/http"
 	"strconv"
 
 	"github.com/quarksgroup/payment-client/client"
@@ -18,16 +19,21 @@ type Balance struct {
 func (c *Client) Balance(ctx context.Context) (*Balance, *client.Response, error) {
 	endpoint := "standard/v1/users/balance"
 
+	headers := http.Header{
+		"X-country":  []string{c.Country},
+		"X-Currency": []string{c.Currency},
+	}
+
 	out := new(balanceResponse)
 
-	res, err := c.do(ctx, "GET", endpoint, nil, out, nil, true)
+	res, err := c.do(ctx, "GET", endpoint, nil, out, headers, true)
 
 	// this is supposed to be taken care of from inside client.do
 	if !out.Status.Success {
 
 		code, _ := strconv.ParseInt(out.Status.Code, 10, 64)
 
-		return nil, nil, &Error{Code: int(code), Message: out.Status.Message}
+		return nil, nil, &Error{Code: int(code), Message: out.ErrMsg}
 	}
 
 	return convertBalance(out), res, err
@@ -46,6 +52,7 @@ type balanceResponse struct {
 		Result_code string `json:"result_code"`
 		Message     string `json:"message"`
 	} `json:"status"`
+	ErrMsg string `json:"status_message"`
 }
 
 func convertBalance(res *balanceResponse) *Balance {
